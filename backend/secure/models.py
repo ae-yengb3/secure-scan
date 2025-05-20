53662d4f84e6ba7f1514d690f2988d18c4441ee2
crazicoda <crazicoda@gmail.com>
2025-05-20 14:41:08 +0000

    Add 'progress' field to Scan model and update ScanSerializer to include it

--- backend/secure/models.py
+++ backend/secure/models.py
@@ -43,6 +43,7 @@ class Scan(models.Model):
     url = models.CharField(max_length=100)
     status = models.CharField(max_length=100)
     scan_id = models.CharField(max_length=100)
+    progress = models.IntegerField(default=0)
     start_time = models.DateTimeField(auto_now_add=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
