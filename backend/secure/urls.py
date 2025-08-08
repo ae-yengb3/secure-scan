244f0e6491b4d82f9e67bdfbb2c28db1add1f52c
crazicoda <crazicoda@gmail.com>
2025-08-08 20:45:13 +0000

    refresh token

--- backend/secure/urls.py
+++ backend/secure/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('create/user/', views.create_user, name='create-user'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', views.get_user, name='get-user'),
     path('start/scan/', views.start_scan, name='start-scan'),
     path('scans/', views.get_scans, name='get-scan'),
